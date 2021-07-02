b8eb6c82239e8fa2c5b131f3a14db3821986800a
Anton <eliseev.anton96@yandex.ru>
2021-07-02 14:29:25 +0300

    ready to heroku

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
-const PORT = 3000;
+const PORT = process.env.PORT;
 const app = express();
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const path = require('path');
 const hbs = require('hbs');
 
@@ -11,7 +12,7 @@ const { categoryRouter } = require('./src/routes/category.router');
 app.set('view engine', 'hbs');
 hbs.registerPartials(path.join(__dirname, 'src', 'views', 'partials'));
 app.set('views', path.join(__dirname, 'src', 'views'));
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
